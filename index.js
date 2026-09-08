39a87ab374165c35ce726e914a1ae3b205fc5892
abdul-dulal <69429521+abdul-dulal@users.noreply.github.com>
2026-09-08 15:26:19 +0600

    Add help route to express application

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello Github Action");
 });
 
+app.get("/help", (req, res) => {
+  res.send("This is the help page");
+});
+
 if (require.main === module) {
   app.listen(3000, () => {
     console.log("Server is running on port 3000");
